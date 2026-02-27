f6f50639955601afa166b13750a07c58c7b8f6d4
Janis Harms <janis.harms88@bluewin.ch>
2026-02-27 09:41:05 +0100

    docker file in right place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 
 
 function App() {
-
   return (
-    <>
-      Hi
-    </>
+    <div className="flex items-center justify-center">
+      <div className="flex flex-row gap-4">
+        <div className="bg-primary w-32 h-32 flex items-center justify-center rounded">
+          Box
+        </div>
+        <div className="bg-primary-light w-32 h-32 flex items-center justify-center rounded">
+          Box
+        </div>
+        <div className="bg-primary-light-fine w-32 h-32 flex items-center justify-center rounded">
+          Box
+        </div>
+
+
+        <div className="border border-primary-light text-primary w-32 h-32 flex items-center justify-center rounded">
+          Box
+        </div>
+        <div className="border border-primary-light text-primary-text w-32 h-32 flex items-center justify-center rounded">
+          Box 
+        </div>
+        <div className="border border-primary-light text-primary-light w-32 h-32 flex items-center justify-center rounded">
+          Box 
+        </div>
+      </div>
+    </div>
   )
 }
 
