fa6f6017fc72e36b7308fd22267a2708ff1fa440
Janis Harms <janis.harms88@bluewin.ch>
2026-03-24 14:37:27 +0100

    added Statistics to Skills page

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,6 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import SkillCard from "../components/CardComponents/SkillCard"
 import skills from "../data/skills.json"
 import RevealOnView from "../components/RevealOnView";
+import Statistics from '../components/Statistics';
 
 const Skills = () => {
   return (
@@ -16,6 +17,8 @@ const Skills = () => {
         <meta property='og:description' content='An overview of the technologies and tools I have learned and worked with, including web development, frameworks, and more.' />
         <meta property='og:image' content='https://janisharms.ch/logo_janis_og.png' />
       </Helmet>
+
+      <Statistics />
       <div className='projects flex flex-col gap-1 align-items-center justify-center text-center mt-6 md:mt-10 mx-4 md:mx-10'>
           <RevealOnView className='text-3xl md:text-4xl font-bold text-primary-white'>
             Personal Skills
